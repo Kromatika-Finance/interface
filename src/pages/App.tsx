ce6330ebb8ffa89d3975b13eda705b750e66f685
0xelMag0 <astefaneli97@gmail.com>
2023-03-27 01:05:58 +0200

    update

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -78,43 +78,6 @@ const TopLevelModals = () => {
   return <AddressClaimModal isOpen={open} onDismiss={toggle} />
 }
 
-const Application = () => (
-  <AppWrapper>
-    <HeaderWrapper>
-      <Header />
-    </HeaderWrapper>
-    <BodyWrapper>
-      <Popups />
-      <Polling />
-      <TopLevelModals />
-      <Switch>
-        <Route exact strict path="/limitorder/:outputCurrency" component={RedirectToLimitOrder} />
-        <Route exact strict path="/limitorder" component={LimitOrder} />
-        <Route exact strict path="/swap/:outputCurrency" component={RedirectToMarket} />
-        <Route path="/swap" component={Market} />
-        <Route exact strict path="/pool" component={Pool} />
-        <Route exact strict path="/pool/:tokenId" component={PositionPage} />
-        <Route exact strict path="/stake/:tokenId" component={StakingModal} />
-        <Route exact strict path="/unstake/:tokenId/remove" component={StakingModal} />
-        <Route exact strict path="/remove/v2/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
-        <Route exact strict path="/remove/:tokenId" component={RemoveLiquidityV3} />
-        <Route exact strict path="/add/:currencyIdA?/:currencyIdB?/:feeAmount?" component={RedirectDuplicateTokenIds} />
-        <Route component={RedirectPathToLimitOrderOnly} />
-        <Route component={RedirectPathToSwapOnly} />
-      </Switch>
-    </BodyWrapper>
-  </AppWrapper>
-)
-
-const Widget = () => (
-  <AppWrapper>
-    <Switch>
-      <Route path="/swap-widget/dark" component={SwapWidget} />
-      <Route path="/swap-widget/light" component={SwapWidget} />
-    </Switch>
-  </AppWrapper>
-)
-
 export default function App() {
   const { pathname } = useLocation()
   const isApp = !pathname.includes('swap-widget')
@@ -128,7 +91,8 @@ export default function App() {
       <Route component={ApeModeQueryParamReader} />
       <Web3ReactManager>
         <Switch>
-          <Route exact path="/swapwidget/dark" component={SwapWidget} />
+          <Route exact strict path="/swapwidget/dark" component={SwapWidget} />
+          <Route path="/swapwidget" component={SwapWidget} />
           <Route exact path="/swapwidget/light" component={SwapWidget} />
           <AppWrapper>
             <HeaderWrapper>
